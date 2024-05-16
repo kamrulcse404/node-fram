fd5314adf8e2a070883756dff49ce4d86ab59d2d
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-16 16:40:54 +0600

    using modules 2 our own modules

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%product_name%}/g, product.productName);
-  output = output.replace(/{%image%}/g, product.image);
-  output = output.replace(/{%price%}/g, product.price);
-  output = output.replace(/{%from%}/g, product.from);
-  output = output.replace(/{%nutrients%}/g, product.nutrients);
-  output = output.replace(/{%quantity%}/g, product.quantity);
-  output = output.replace(/{%description%}/g, product.description);
-  output = output.replace(/{%id%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%not_organic%}/g, "not-organic");
-  }
-
-  return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
