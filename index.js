3c4ffa815d93df9d21e801c4526a8b7b2ebcb4e3
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-16 11:19:49 +0600

    simple api

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
-  
-  if (pathName === '/' || pathName === '/overview') {
+
+  if (pathName === "/" || pathName === "/overview") {
     res.end("This is the overview page");
-  } else if (pathName === '/product') {
+  } else if (pathName === "/product") {
     res.end("This is the product page");
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+
+      const productData = JSON.parse(data);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+      // res.end(JSON.stringify(productData));
+    });
   } else {
     res.writeHead(404, {
-      'Content-Type': "text/html",
-      'my-content-type': "Hello Hasan",
+      "Content-Type": "text/html",
+      "my-content-type": "Hello Hasan",
     });
-    res.end("This page was not found");
+    res.end("<h1>This page was not found</h1>");
   }
 });
 
