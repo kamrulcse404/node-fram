e3964d93a67557d0f83d2dc6cba028d86514a7ab
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-16 11:27:13 +0600

    update api

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const http = require("http");
 const fs = require("fs");
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("This is the overview page");
   } else if (pathName === "/product") {
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("This is the product page");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
-        return;
-      }
-
-      const productData = JSON.parse(data);
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(data);
-      // res.end(JSON.stringify(productData));
-    });
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(dataObj));
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
